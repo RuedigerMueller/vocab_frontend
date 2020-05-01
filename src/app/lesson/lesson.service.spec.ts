b560356f0e5380c1ba1e2d892733fab64cbda9a3
BuildTools <unconfigured@null.spigotmc.org>
2020-05-01 15:36:14 +0200

    completed lesson service tests

--- src/app/lesson/lesson.service.spec.ts
+++ src/app/lesson/lesson.service.spec.ts
@@ -1,35 +1,81 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController, TestRequest } from '@angular/common/http/testing';
 import { HttpClient } from '@angular/common/http';
 
 import { LessonService } from './lesson.service';
-import { TileTextDirective } from '@fundamental-ngx/core/lib/tile/tile-text/tile-text.directive';
-import { i18nMetaToDocStmt } from '@angular/compiler/src/render3/view/i18n/meta';
-import { IterableDiffers } from '@angular/core';
+import { Lesson } from './lesson.service.interface';
+import { environment } from 'src/environments/environment';
+
+const testLessons: Lesson[] = [
+  { id: 2, user: 'User1', title: 'Unit 2', language_a: 'English', language_b: 'Deutsch', numberVocables: 30, numberDueVocables: 2 },
+  { id: 4, user: 'User1', title: 'Unidad 1', language_a: 'Español', language_b: 'Deutsch', numberVocables: 0, numberDueVocables: 0 },
+  { id: 5, user: 'User1', title: 'Unidad 1', language_a: 'Español', language_b: 'Deutsch', numberVocables: 0, numberDueVocables: 0 },
+];
 
 describe('LessonService', () => {
-  let httpClient: HttpClient;
   let httpTestingController: HttpTestingController;
+  let lesssonService: LessonService;
+  const lessonsURI = 'lessons/';
 
-  let service: LessonService;
+  const requestCheck = async (url: string, method: string, testData: any) => {
+    const req: TestRequest = httpTestingController.expectOne(url);
+    expect(req.request.method).toBe(method);
 
+    req.flush(testData);
+    httpTestingController.verify();
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ HttpClientTestingModule ],
+      imports: [HttpClientTestingModule],
     });
-    service = TestBed.inject(LessonService);
-
-    httpClient = TestBed.inject(HttpClient);
+    lesssonService = TestBed.inject(LessonService);
     httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(lesssonService).toBeTruthy();
   });
 
-  it('should get lessons');
-  it('should create a lesson');
-  it('should delete a lesson');
-  it('should update a lesson');
+  it('should get lessons', () => {
+    const expectedLessons: Lesson[] = testLessons;
+    lesssonService.getLessons().subscribe((lessons: Lesson[]) => {
+      expect(lessons.length).toBe(expectedLessons.length);
+    });
+
+    requestCheck(environment.backendUrl + lessonsURI, 'GET', expectedLessons);
+  });
+
+  it('should get a lesson', () => {
+    const expectedLesson: Lesson = testLessons[0];
+
+    lesssonService.getLesson(expectedLesson.id.toString()).subscribe(
+      lesson => expect(lesson).toEqual(expectedLesson, 'expected lesson'),
+      fail
+    );
+
+    requestCheck(environment.backendUrl + lessonsURI + expectedLesson.id, 'GET', expectedLesson);
+  });
+
+  it('should update a lesson', () => {
+    const updatedLesson: Lesson = testLessons[0];
+
+    lesssonService.updateLesson(updatedLesson.id.toString(), updatedLesson).subscribe(
+      () => { },
+      fail
+    );
+
+    requestCheck(environment.backendUrl + lessonsURI + updatedLesson.id, 'PATCH', updatedLesson);
+  });
+
+  it('should delete a lesson', () => {
+    const deletedLesson: Lesson = testLessons[0];
+
+    lesssonService.deleteLesson(deletedLesson.id.toString()).subscribe(
+      () => { },
+      fail
+    );
+
+    requestCheck(environment.backendUrl + lessonsURI + deletedLesson.id, 'DELETE', deletedLesson);
+  });
 });
