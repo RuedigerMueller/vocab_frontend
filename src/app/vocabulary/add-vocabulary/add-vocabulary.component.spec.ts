3c4d6c8e17e96fe63514712ee0046e61bcd65e62
BuildTools <unconfigured@null.spigotmc.org>
2020-05-09 16:30:01 +0200

    fixed test setup

--- src/app/vocabulary/add-vocabulary/add-vocabulary.component.spec.ts
+++ src/app/vocabulary/add-vocabulary/add-vocabulary.component.spec.ts
@@ -1,16 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddVocabularyComponent } from './add-vocabulary.component';
+import { HttpClient } from '@angular/common/http';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('AddVocabularyComponent', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
   let component: AddVocabularyComponent;
   let fixture: ComponentFixture<AddVocabularyComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddVocabularyComponent ]
+      declarations: [ AddVocabularyComponent ],
+      imports: [
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes([]),
+      ],
     })
     .compileComponents();
+
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
   }));
 
   beforeEach(() => {
--- src/app/vocabulary/edit-vocabulary/edit-vocabulary.component.spec.ts
+++ src/app/vocabulary/edit-vocabulary/edit-vocabulary.component.spec.ts
@@ -1,16 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditVocabularyComponent } from './edit-vocabulary.component';
+import { HttpClient } from '@angular/common/http';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('EditVocabularyComponent', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
   let component: EditVocabularyComponent;
   let fixture: ComponentFixture<EditVocabularyComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EditVocabularyComponent ]
+      declarations: [ EditVocabularyComponent ],
+      imports: [
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes([]),
+      ],
     })
     .compileComponents();
+
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
   }));
 
   beforeEach(() => {
