1612f5b067dc17c092fb6b29adc25551387bc3a3
BuildTools <unconfigured@null.spigotmc.org>
2020-05-31 10:23:48 +0200

    redirecting to login on 401 from backend

--- src/app/helpers/auth-guard.service.ts
+++ src/app/helpers/auth-guard.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from './auth.service';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +10,15 @@ export class AuthGuardService implements CanActivate {
 
   constructor(private authService: AuthService, private router: Router) { }
 
-  canActivate() {
-    const currentUser = this.authService.currentUserValue;
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const currentUser: User = this.authService.currentUserValue;
     if (currentUser) {
       // logged in so return true
       return true;
     }
 
     // not logged in so redirect to login page with the return url
-    // this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
-    this.router.navigate(['/login']);
+    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
     return false;
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 import { AuthService } from '../helpers/auth.service';
 import { first } from 'rxjs/operators';
 
@@ -10,37 +10,44 @@ import { first } from 'rxjs/operators';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
   userEmail: string;
   userPassword: string;
   loginForm: FormGroup;
+  returnUrl: string;
+  error = '';
 
-  constructor(private authService: AuthService, private router: Router, public fb: FormBuilder) { }
+
+  constructor(private authService: AuthService, private route: ActivatedRoute, private router: Router, public fb: FormBuilder) {
+    // redirect to home if already logged in
+    if (this.authService.currentUserValue) {
+      this.router.navigate(['/']);
+    }
+  }
 
   ngOnInit() {
     this.loginForm = this.fb.group({
-      eMail: ['john@example.com'],
-      password: ['changeme']
+      eMail: ['john@example.com', Validators.required],
+      password: ['changeme', Validators.required],
     });
 
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams.returnUrl || '/';
   }
 
   login() {
-    /* this.authService.validate(this.loginForm.value.eMail, this.loginForm.value.password)
-      .then((response) => {
-        this.authService.setUserInfo(response);
-        this.router.navigate(['lessons']);
-      });
-  } */
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+
     this.authService.login(this.loginForm.value.eMail, this.loginForm.value.password)
       .pipe(first())
       .subscribe(
         data => {
-          this.router.navigate(['lessons']);
+          this.router.navigate([this.returnUrl]);
         },
         error => {
-          /* this.error = error;
-          this.loading = false. */
+          this.error = error;
         }
       );
   }
