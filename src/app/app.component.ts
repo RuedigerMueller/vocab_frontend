302cae826aa954b21dfe7f6b4f02a42ffd64b847
BuildTools <unconfigured@null.spigotmc.org>
2020-06-05 16:33:01 +0200

    adding logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { LessonService } from './services/lesson.service';
 import { User } from './models/user.model';
 import { Router } from '@angular/router';
 import { AuthService } from './helpers/auth.service';
+import { ShellbarUser, ShellbarUserMenu } from '@fundamental-ngx/core';
 
 @Component({
   selector: 'app-root',
@@ -19,8 +20,24 @@ export class AppComponent {
     private router: Router,
     private authenticationService: AuthService
   ) {
-    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+    this.authenticationService.currentUser.subscribe((user: User) => {
+      this.currentUser = user;
+      if ( this.currentUser) {
+        this.shellbarUser.initials = user.firstName[0] + user.lastName[0];
+      }
+    });
   }
+
+  shellbarUser: ShellbarUser = {
+    initials: '',
+    colorAccent: 11
+  };
+
+  userMenu: ShellbarUserMenu[] = [
+    // { text: 'Settings', callback: this.settingsCallback },
+    { text: 'Logout', callback: this.logout.bind(this) }
+  ];
+
 
   logout() {
     this.authenticationService.logout();
