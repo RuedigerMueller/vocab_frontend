e23591e9b545852dfaa0603b63206595e6072b59
BuildTools <unconfigured@null.spigotmc.org>
2020-06-20 18:34:06 +0200

    finalizing app compontent tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,8 +13,9 @@ import { AppComponent } from './app.component';
 import { AuthService } from './helpers/auth.service';
 import { User } from './models/user.model';
 import { frontend } from './resource.identifiers';
+import { ExpectedConditions } from 'protractor';
 
-describe('AppComponent', () => {
+describe('AppComponent - logged in', () => {
   let httpClient: HttpClient;
   let httpTestingController: HttpTestingController;
   let fixture: ComponentFixture<AppComponent>;
@@ -85,11 +86,6 @@ describe('AppComponent', () => {
   });
 
   describe('should have required actions', () => {
-    xit('should not have user menu if no user is logged in', () => {
-      /* const button: HTMLButtonElement = fixture.nativeElement.querySelector('#edit-lesson-saveButton');
-      expect(button.textContent).toContain('Save'); */
-    });
-
     it('should have link "Logout" if a user is logged in', () => {
       expandUserMenu();
       const listElement: HTMLLIElement = fixture.nativeElement.querySelector('li.fd-menu__item');
@@ -107,4 +103,67 @@ describe('AppComponent', () => {
       expect(location.path()).toBe(`/${frontend.login}`, 'should nav to login');
     }));
   });
+});
+
+describe('AppComponent - logged out', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  let location: Location;
+  let router: Router;
+
+  let authCurrentUserSpy: any;
+  const testUser: User = userTestData[0];
+
+  let authService: any;
+
+  const expandUserMenu = () => {
+    const expandableButton: HTMLButtonElement = fixture.nativeElement.querySelector('.fd-identifier.fd-identifier--xs');
+    expandableButton.click();
+    fixture.detectChanges();
+  };
+
+  beforeEach(async(() => {
+    authService = jasmine.createSpyObj('AuthService', ['getCurrentUser', 'logout']);
+    authCurrentUserSpy = authService.getCurrentUser.and.returnValue(of(null));
+
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes(routes),
+        ShellbarModule,
+        /* // required because we are navigating to the loginComponent in a test and this required it
+        ReactiveFormsModule, */
+      ],
+     providers: [
+        { provide: AuthService, useValue: authService }
+      ],
+    }).compileComponents();
+
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  }));
+
+  beforeEach(() => {
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  describe('should create application', () => {
+    it('should create the app', () => {
+      expect(app).toBeTruthy();
+    });
+  });
+
+  describe('when logged out it', () => {
+    it('should not have user menu', fakeAsync(() => {
+      const userMenu: HTMLLIElement = fixture.nativeElement.querySelector('div.fd-user-menue');
+      expect(userMenu).toBeNull();
+    }));
+  })
 });
