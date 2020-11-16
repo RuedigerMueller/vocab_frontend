7221722c140f9d378e732a9822168d68d19e795f
BuildTools <unconfigured@null.spigotmc.org>
2020-11-16 19:25:58 +0100

    Attempting to go for one build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backendUrl: 'https://vocabtsbe.herokuapp.com/backend',
-  //backendUrl: '/backend',
+  //backendUrl: 'https://vocabtsbe.herokuapp.com/backend',
+  backendUrl: '/backend',
 };
