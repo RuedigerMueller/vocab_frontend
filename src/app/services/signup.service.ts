6f01c3a881d13789e965c955766e64f14e190b83
BuildTools <unconfigured@null.spigotmc.org>
2020-12-01 17:34:13 +0100

    clean-up commented out code

--- src/app/services/signup.service.ts
+++ src/app/services/signup.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ValidationErrors } from '@angular/forms';
 import { Observable, throwError } from 'rxjs';
--- src/app/validators/eMailValidator.ts
+++ src/app/validators/eMailValidator.ts
@@ -1,26 +1,7 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
 import { Directive } from '@angular/core';
 import { AbstractControl, AsyncValidator, AsyncValidatorFn, NG_ASYNC_VALIDATORS, ValidationErrors } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { User } from '../models/user.model';
-import { backend, baseURL } from '../resource.identifiers';
 import { SignupService } from '../services/signup.service';
-import { UserService } from '../services/user.service';
-
-/* export function EMailValidator(http: HttpClient): AsyncValidatorFn {
-    return (control: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> => {
-        const params = new HttpParams().set('email', control.value);
-        const obs = http.get<User>(`${baseURL}/${backend.users}`, { params })
-            .pipe(
-                map((user) => {
-                    // null no error, object for error
-                    return !user ? null : { eMailTaken: true };
-                })
-            );
-        return obs;
-    };
-} */
 
 export function EMailValidator(signupService: SignupService): AsyncValidatorFn {
     return (control: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> => {
@@ -39,7 +20,6 @@ export class EMailValidatorDirective implements AsyncValidator {
     constructor(private signupService: SignupService) { }
 
     validate(control: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
-        // return EMailValidator(this.http)(control);
         return EMailValidator(this.signupService)(control);
     }
 }
